8f68e2318e0e2562093d660223bae377c0263436
AhmadMunazir <Alyatakin29@gmail.com>
2024-09-17 15:39:15 +0800

    bab4 strukturkontrol (nilai ujian, fizzbuzz, penjumlahan

---
+++ bab4-struktur-kontrol/nilai_ujian.php
@@ -0,0 +1,13 @@
+<?php 
+$nilai = 85; 
+if ($nilai >= 90) { 
+ echo "Nilai: A"; 
+} elseif ($nilai >= 80) { 
+ echo "Nilai: B"; 
+} elseif ($nilai >= 70) { 
+ echo "Nilai: C"; 
+} elseif ($nilai >= 60) { 
+ echo "Nilai: D";
+} else {
+echo "Nilai: F";
+}
---
+++ bab4-struktur-kontrol/penjumlahan.php
@@ -0,0 +1,8 @@
+<?php
+$jumlah = 0; 
+$i = 1;
+while ($i <= 50) { 
+ $jumlah += $i;
+ $i++;
+}
+echo "Jumlah dari 1 hingga 50 adalah $jumlah."; 
