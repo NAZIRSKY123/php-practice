e439e53763ef6f3def24374eb2d3c656f974b0db
AhmadMunazir <Alyatakin29@gmail.com>
2024-09-17 15:23:55 +0800

    bab2 variabel dan konversi suhu

---
+++ bab2 variabel-tipe-data/konversi_suhu.php
@@ -0,0 +1,4 @@
+<?php
+$fahrenheit = 98;
+$celsius = ($fahrenheit - 32) * 5 / 9;
+echo "$fahrenheit derajat Fahrenheit sama dengan $celsius derajat Celsius."; 
---
+++ bab2 variabel-tipe-data/variabel.php
@@ -0,0 +1,6 @@
+<?php
+$nama= "john";
+$umur= 25;
+$alamat= "Jakarta";
+
+echo "Nama saya adalah $nama, umur saya $umur tahun, dan saya tinggal di $alamat.";
