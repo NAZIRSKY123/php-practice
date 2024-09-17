ad3f1c47957978d64ae760166be2a5f32334d465
AhmadMunazir <Alyatakin29@gmail.com>
2024-09-17 15:30:53 +0800

    bab3 operator aritmatika dan logika

---
+++ bab3-operator-aritmatika/operator_aritmatika.php
@@ -0,0 +1,4 @@
+<?php
+$radius = 7;
+$luas = pi() * $radius * $radius;
+echo "Luas lingkaran dengan radius $radius adalah $luas.";
---
+++ bab3-operator-aritmatika/operator_logika.php
@@ -0,0 +1,7 @@
+<?php 
+$angka = 10; 
+if ($angka % 2 == 0) {
+ echo "$angka adalah bilangan genap.";
+} else {
+ echo "$angka adalah bilangan ganjil.";
+}
